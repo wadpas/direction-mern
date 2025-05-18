98169ed84981d15ea8124e56e9fe97342c167897
wadpas <123688961+wadpas@users.noreply.github.com>
2025-05-18 13:41:30 +0300

    controller enhanced

--- server/controllers/products.ts
+++ server/controllers/products.ts
@@ -3,12 +3,76 @@ import { Request, Response } from 'express'
 import Product from '../models/product.js'
 
 export const getProducts = async (req: Request, res: Response): Promise<any> => {
-  try {
-    const products = await Product.find({})
-    res.status(200).json({ products })
-  } catch (error) {
-    res.status(500).json({ error })
+  const { featured, company, name, sort, fields, numericFilters } = req.query
+
+  interface queryObjectType {
+    featured?: boolean
+    company?: string
+    name?: Object
+    price?: Object
+    rating?: Object
   }
+  const queryObject: queryObjectType = {}
+
+  if (featured) {
+    queryObject.featured = featured as unknown as boolean
+  }
+
+  if (company) {
+    queryObject.company = company as string
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+
+    let filters = (numericFilters as string).replace(
+      regEx,
+      (match) => `-${operatorMap[match as keyof typeof operatorMap]}-`
+    )
+
+    const options = ['price', 'rating']
+    filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        //@ts-ignore
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+  }
+
+  let results = Product.find(queryObject)
+
+  if (sort) {
+    const sortList = sort.toString().split(',').join(' ')
+    results = results.sort(sortList)
+  } else {
+    results = results.sort('-createdAt')
+  }
+
+  if (fields) {
+    const fieldsList = fields.toString().split(',').join(' ')
+    results = results.select(fieldsList)
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  results = results.skip(skip).limit(limit)
+  const products = await results
+
+  res.status(200).json({ products })
 }
 
 export const createProduct = async (req: Request, res: Response): Promise<any> => {
